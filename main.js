0f58c95264fa40ce3ffee01acaed4240b139d165
Davis-st <davixd86pro@gmail.com>
2026-03-23 21:10:56 -0600

    Remove test comment from Supabase configuration

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { ColaPedidos } from './Estructuras/ColaPedidos.js';
 
-// --- CONFIGURACIÓN DE SUPABASE ---PRUEBA DE GIT
+// --- CONFIGURACIÓN DE SUPABASE ---
 const SUPABASE_URL = 'https://nybdoaclmzdfqeaefgxx.supabase.co'; 
 const SUPABASE_KEY = 'sb_publishable_JIUJgw-34pVpDNE8yxdNlQ_xz3vSxzu';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
